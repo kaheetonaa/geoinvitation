4c4f0390e910b59b2d40dc8d4c5ed42ce5f3e20d
Kaheetonaa <kaheetonaa@gmail>
2024-12-31 17:08:07 +0100

    update invitation

--- src/App.svelte
+++ src/App.svelte
@@ -10,6 +10,9 @@
   import Geolocation from './lib/img/Geolocation.png';
   import GreatCircle from 'great-circle';
   var currentPos={lng:0,lat:0};
+  let dist=-1;
+  let button="Ẩn🔻";
+ 
 const markers=[
     {
       lngLat: [105.845411,21.051686],
@@ -24,9 +27,22 @@ function checkGeo() {
   }
 }
 
+
   function showPosition(position) {
     currentPos={lng:position.coords.longitude,lat:position.coords.latitude};
-    console.log(GreatCircle.distance(currentPos.lat,currentPos.lng, 21.051686, 105.845411))
+    dist=GreatCircle.distance(currentPos.lat,currentPos.lng, 21.051686, 105.845411);
+}
+
+
+function movedown() {
+  let element = document.getElementById("invitation");
+  console.log(element)
+  element.classList.toggle('movedown')
+  if (button=="Ẩn🔻") {
+    button = "Hiện🔺"
+  } else {
+    button = "Ẩn🔻"
+  }
 }
 
 setInterval(checkGeo, 5000);
@@ -45,7 +61,7 @@ setInterval(checkGeo, 5000);
     <!-- Unlike the custom marker example, default markers do not have mouse events,
     and popups only support the default openOn="click" behavior -->
     <Marker {lngLat}>
-    <img src={Logo} width="150px"/>
+    <img src={Logo} width="100px"/>
       <Popup offset={[0, -75]}>
         <div class="text-lg font-bold">{name}</div>
       </Popup>
@@ -58,21 +74,75 @@ setInterval(checkGeo, 5000);
       </Popup>
     </Marker>
 </MapLibre>
-<p id="desciption">Hello</p>
+{#if dist!=-1}
+<div style="z-index:205;position:absolute;right:5vw;top:5vw;width:55%">Khoảng cách giữa bạn và đám cưới Tố Nga và Quang Huy chỉ là {dist}km</div>
+{/if}
+
+<div id='invitation'>
+<button style="z-index:207;position:absolute;right:0px;top:-20px;" onclick={movedown}>{button}</button>
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="{G1}" alt="Avatar" style="height:100%">
+    </div>
+    <div class="flip-card-back">
+     <div style="position:absolute;text-align:center;top:20%;z-index:210;color:red;width:100%">Các bạn của tui</div>
+     <img src="{G2}" alt="Avatar" style="height:100%">
+    
+    </div>
+  </div>
+</div>
 
 <style>
-	main {
-		width: 100%;
-		height: 100vh;
-		position: relative;
-	}
-  #map{
-    z-index:-1;
-  }
-  #description{
-    position:absolute;
-    left:0;
-    top:0;
-    z-index:99;
-  }
+/* The flip card container - set the width and height to whatever you want. We have added the border property to demonstrate that the flip itself goes out of the box on hover (remove perspective if you don't want the 3D effect */
+#invitation {
+  position:absolute;
+  background-color: transparent;
+  width: 30vh;
+  height: 50vh;
+  perspective: 1000px; /* Remove this if you don't want the 3D effect */
+  bottom:5vh;
+  left: 5%;
+}
+
+:global(.movedown){
+  transform: translate(0px, 50vh);
+  transition-duration: 2s;
+}
+
+/* This container is needed to position the front and back side */
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+/* Do an horizontal flip when you move the mouse over the flip box container */
+#invitation:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+/* Position the front and back side */
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
+}
+
+/* Style the front side (fallback if image is missing) */
+.flip-card-front {
+  background-color: #bbb;
+  color: black;
+}
+
+/* Style the back side */
+.flip-card-back {
+  background-color: dodgerblue;
+  color: white;
+  transform: rotateY(180deg);
+}
 </style>
